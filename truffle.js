003434113b2e88c8d46adf356e1f7a227d53cacb
Chi Kei Chan <chikeichan@gmail.com>
2019-01-08 16:43:35 -0800

    done

--- truffle.js
+++ truffle.js
@@ -3,9 +3,10 @@ const Web3 = require("web3");
 const web3 = new Web3();
 const WalletProvider = require("truffle-wallet-provider");
 const Wallet = require('ethereumjs-wallet');
+const keystore = require('fs').readFileSync('./keystore').toString()
 
-var oxygenPrivateKey = new Buffer('0x8b31dfdf8a468269bdc865cb959c6e4f0aacc208e47a2052e1aeeb21b9d0f674', "hex")
-var oxygenWallet = Wallet.fromPrivateKey(oxygenPrivateKey);
+// var oxygenPrivateKey = new Buffer('', "hex")
+var oxygenWallet = Wallet.fromV3(keystore, 'distributedworld1');
 var oxygenProvider = new WalletProvider(oxygenWallet, "http://localhost:8501");
 
 
@@ -26,11 +27,11 @@ module.exports = {
       gasPrice: web3.toWei("20", "gwei"),
       network_id: "3",
     },
-    mainnet: {
-      provider: mainNetProvider,
-      gas: 4600000,
-      gasPrice: web3.toWei("20", "gwei"),
-      network_id: "1",
-    }
+    // mainnet: {
+    //   provider: mainNetProvider,
+    //   gas: 4600000,
+    //   gasPrice: web3.toWei("20", "gwei"),
+    //   network_id: "1",
+    // }
   }
 };
