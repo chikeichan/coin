19c2d275ffa6b08bdcadd23fa49b7d69f9c6fa8e
Chi Kei Chan <chikeichan@gmail.com>
2019-01-08 16:46:49 -0800

    Add compiler version

--- truffle.js
+++ truffle.js
@@ -33,5 +33,18 @@ module.exports = {
     //   gasPrice: web3.toWei("20", "gwei"),
     //   network_id: "1",
     // }
+  },
+    compilers: {
+    solc: {
+      version: "0.4.25"    // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      // settings: {          // See the solidity docs for advice about optimization and evmVersion
+      //  optimizer: {
+      //    enabled: false,
+      //    runs: 200
+      //  },
+      //  evmVersion: "byzantium"
+      // }
+    }
   }
 };
